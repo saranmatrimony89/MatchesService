40fc23714875c73427a7938cafac9b6a9b85fc7a
Saravanan Chinnadurai <saranchinnadurai@gmail.com>
2022-02-12 17:55:11 +0800

    added refre to fecth updated values

--- src/main/java/com/example/demo/controller/MemberRestController.java
+++ src/main/java/com/example/demo/controller/MemberRestController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import com.example.demo.runner.RegisterConsumer;
 
 @RestController
 @RequestMapping("/member")
+@RefreshScope    // To get the latest value from git server
 public class MemberRestController {
 	@Autowired
 	RegisterConsumer consumer;
